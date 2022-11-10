044114779ef0e04aa1d49dc18b5d22ffdae8aa98
showyha08 <over.and.uver@gmail.com>
2022-11-10 23:52:38 +0900

    tax_calculation

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,66 @@
-console.log("Node Path = " + process.argv[0]);
-console.log("script file path =" + process.argv[1]);
+import { NumericVersion } from "../../../node_modules/aws-sdk/clients/inspector";
+import { Textract } from "../../../node_modules/aws-sdk/index";
+
+console.log("Node path = " + process.argv[0]);
+console.log("script file apath = " + process.argv[1]);
 
 const data: number[] = [];
 for (var i = 2; i < process.argv.length; i++) {
   data.push(Number(process.argv[i]));
 }
-console.log(data);
+
+console.log("parameters: " + data);
+
+const f = aggregate();
 
 for (let item of data) {
-  const res = primeFactor(item);
-  console.log(item + " =  " + res);
+  const res = f(item);
+  console.log(res);
 }
 
-function primeFactor(a: number): number[] {
-  const v: number[] = [];
-  let x = a;
-  let n = 2;
-  while (x > n) {
-    if (x % n == 0) {
-      x = x / n;
-      v.push(n);
-    } else {
-      n += n == 2 ? 1 : 2;
-    }
-  }
-  v.push(x);
-  return v;
+function aggregate(): (n: number) => [number, number, number, number, number] {
+  let total = 0;
+  let totalp = 0;
+
+  let totalt = 0;
+  return (n: number): [number, number, number, number, number] => {
+    total += n;
+    let tax = Math.floor(n - n / 1.1);
+    totalp += n - tax;
+    totalt += tax;
+    return[n, tax, total, totalp, totalt];
+  };
 }
+
+// console.log("Node Path = " + process.argv[0]);
+// console.log("script file path =" + process.argv[1]);
+
+// const data: number[] = [];
+// for (var i = 2; i < process.argv.length; i++) {
+//   data.push(Number(process.argv[i]));
+// }
+// console.log(data);
+
+// for (let item of data) {
+//   const res = primeFactor(item);
+//   console.log(item + " =  " + res);
+// }
+
+// function primeFactor(a: number): number[] {
+//   const v: number[] = [];
+//   let x = a;
+//   let n = 2;
+//   while (x > n) {
+//     if (x % n == 0) {
+//       x = x / n;
+//       v.push(n);
+//     } else {
+//       n += n == 2 ? 1 : 2;
+//     }
+//   }
+//   v.push(x);
+//   return v;
+// }
 
 // window.addEventListener("load", function (event) {
 //   var p = document.querySelector("#target");
