b2359ee8a85f15132490dc2716175737115a110b
showyha08 <over.and.uver@gmail.com>
2022-11-26 15:41:22 +0900

    add_dentaku

--- react_typescript_app/src/App.tsx
+++ react_typescript_app/src/App.tsx
@@ -1,47 +1,66 @@
-import { useState, ReactElement } from "react";
-import "./App.css";
+import { ChangeEvent, KeyboardEvent, ReactElement, useState } from 'react'
+import './App.css'
 
-function App(): ReactElement {
-  const [counter, setCounter] = useState(0)
-  const doAction = () => {
-    setCounter(counter + 1)
+function App(): ReactElement{
+  const [val, setVal] = useState(0)
+  const [data, setData] = useState<number[]>([])
+
+  const doChange = (event: ChangeEvent): void => {
+    const ob = event.target as HTMLInputElement
+    const re = Number(ob.value)
+    setVal(re)
   }
+
+  const doAction = (): void => {
+    const arr: number[] = []
+    for (let item of data)
+      arr.push(item)
+    arr.push(val)
+    setData(arr)
+    setVal(0)
+  }
+
+  const doType = (event: KeyboardEvent): void => {
+    if (event.code == 'Enter') {
+      doAction()
+    }
+  }
+
+  let total = 0
   return (
-      <div>
-        <h1 className="bg-primary text-white p-2">React sample</h1>
-        <div className="container"></div>
+    <div>
+      <h1 className="bg-primary text-white p-2">
+        React sample
+      </h1>
+      <div className="container">
         <h2 className="my-3">click button!</h2>
         <div className="alert alert-primary">
           <div className="row px-2">
-          <h3 id="msg" className="col">{counter} click.</h3>
-          <button onClick={doAction}
-            className="btn btn-primary col-2">
-            Click!
-          </button>
+            <input type="number" className="col" onChange={doChange} onKeyPress={doType} value={val} />
+            <button onClick={doAction} className="btn btn-primary col-2">Click</button>
           </div>
         </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                value
+              </th>
+              <th>
+                total
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((v, k) => {
+              total += v
+              return <tr key={k}><td>{v}</td><td>{total}</td></tr>
+            })}
+          </tbody>
+        </table>
       </div>
-      );
+    </div>
+  )
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-export default App;
+export default App
