cdc60ccfa12016682614f9d2ee41d39cac3c5729
showyha08 <over.and.uver@gmail.com>
2022-11-26 21:47:51 +0900

    chapter6_finish

--- vue/vue3_type_app/src/components/HelloWorld.vue
+++ vue/vue3_type_app/src/components/HelloWorld.vue
@@ -1,40 +1,26 @@
-<template>
-    <h1 class="bg-info text-white p-2">{{ msg }}</h1>
-    <div class="container">
-      <h2 class="my-3">number counter.</h2>
-      <div class="alert alert-info">
-        <h3 v-on:click="doAction">{{val}} count.</h3>
-      </div>
-    </div>
-</template>
+<script lang="tsx">
+  import { VNode } from "vue";
+  import { Vue } from "vue-class-component";
 
-<script lang="ts">
-import { Vue } from 'vue-class-component';
+  export default class HelloWorld extends Vue {
+    msg = "Vue sample.";
+    val = 1;
 
-export default class HelloWorld extends Vue {
-  msg!: string
-  val = 1
-
-  doAction():void {
-    this.val += 1
+    doAction(): void {
+      this.val += 1;
+    }
+    render(): VNode {
+      return (
+        <div>
+          <h1 class="bg-info text-white p-2">{this.msg}</h1>
+          <div class="container">
+            <h2 class="my-3">number counter.</h2>
+            <div class="alert alert-info">
+              <h3 onClick={this.doAction}>{this.val}</h3>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
